12510065c4d5413884a08e85eb8d83659cd1e843
ozcelikaliriza-rgb <ozcelikaliriza@gmail.com>
2026-02-23 21:20:18 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kelime-oyunu-15e75abb/', // Repo adın buysa dokunma, farklıysa düzelt.
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
